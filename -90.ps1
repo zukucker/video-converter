3f9d5ad6c9154f2c4805a27dc33715ec22f7d034
zukucker <alexander.froehling@googlemail.com>
2021-08-26 11:56:29 +0200

    init

---
+++ -90.ps1
@@ -0,0 +1,17 @@
+﻿$file = $args[0]
+
+Add-Type -AssemblyName PresentationFramework
+
+$testval = $file.ToString()
+
+# string teilen um später eigenen namen einfügenzu können
+$t = $testval.Substring(0, $testval.Length-4)
+
+# mache kopie um nicht überschreiben zu müssen
+Move-Item $file -Destination $t-old.mp4
+
+# drehe um 90°
+ffmpeg -y -i $t-old.mp4 -c copy -metadata:s:v:0 rotate=90 $file
+
+#lösche backup datei
+Remove-Item $t-old.mp4
